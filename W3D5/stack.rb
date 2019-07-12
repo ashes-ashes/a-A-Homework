cff0dc7aef0565b20075e2ba90b717c81aec4f2a
Ash Todd <ashztodd@gmail.com>
2019-07-11 23:58:11 -0700

    Added Stack class

--- W3D5/stack.rb
+++ W3D5/stack.rb
@@ -1,6 +1,22 @@
 class Stack
+
+    attr_accessor :stack
+
     def initialize
         @stack = []
     end
 
+    def push(el)
+        @stack << el
+    end
+
+    def pop
+        popped = @stack[-1]
+        @stack = @stack[0...-1]
+        popped
+    end
+
+    def peek
+        @stack[-1]
+    end
 end
