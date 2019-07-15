2d704d72bcbf81a8d21867c03eb6d3b93272e8ed
Ash Todd <ashztodd@gmail.com>
2019-07-14 23:48:34 -0700

    added test nodes

--- W4D1/graphnode.rb
+++ W4D1/graphnode.rb
@@ -1,14 +1,27 @@
 class GraphNode
 
-    attr_accessor: 
+    attr_accessor :value, :neighbors
 
     def initialize(value, *neighbors)
         @value = value
         @neighbors = neighbors
     end
 
-    def set_neighbor(neighbor)
-        @neighbors << neighbor
+    def neighbors=(neighbors)
+        @neighbors += neighbors
     end
 
+end
+
+if __FILE__ == $PROGRAM_NAME
+    a = GraphNode.new('a')
+    b = GraphNode.new('b')
+    c = GraphNode.new('c')
+    d = GraphNode.new('d')
+    e = GraphNode.new('e')
+    f = GraphNode.new('f')
+    a.neighbors = [b, c, e]
+    c.neighbors = [b, d]
+    e.neighbors = [a]
+    f.neighbors = [e]
 end
